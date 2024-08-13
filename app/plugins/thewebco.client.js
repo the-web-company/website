4974ef2cd2ecbba2422c4ceb08abea3e8eb74063
Nir Galon <nir@galons.io>
2024-08-13 23:59:16 +0300

    fix: upgrade thewebco/pid package

--- app/plugins/thewebco.client.js
+++ app/plugins/thewebco.client.js
@@ -1,7 +1,7 @@
 import { defineNuxtPlugin } from "#app";
 import { TheWebCo } from "@thewebco/pid";
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const thewebcoClient = new TheWebCo("14ada8cb-136b-442d-91ce-7330c6d5ebbd");
 
   // Make sure that pageviews are captured with each route change
