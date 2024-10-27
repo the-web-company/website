df310ec02622e3df39c11b7506ae53378270bb49
Nir Galon <nir@galons.io>
2024-10-27 18:26:01 +0200

    fix: update pid script

--- app/plugins/thewebco.client.js
+++ app/plugins/thewebco.client.js
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from "#app";
-import { TheWebCo } from "@thewebco/pid";
+import { TheWebCo } from "https://thewebco.cloud/TheWebCo.js";
 
 export default defineNuxtPlugin(() => {
   const thewebcoClient = new TheWebCo("14ada8cb-136b-442d-91ce-7330c6d5ebbd");
